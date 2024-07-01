0dd99e674b9d4fcfd694bf0643b6877b1066f5cc
Abdul Saboor <66201863+abdulsaboor938@users.noreply.github.com>
2024-07-01 20:52:46 +0500

    Added temp files before creating parser

---
+++ commands/c_command.py
@@ -0,0 +1,5 @@
+def handle_c_command(date, dir_path):
+    """
+    Execute the 'c' command with the given date (YYYY/MM)
+    """
+    w
---
+++ commands/e_command.py
@@ -0,0 +1,8 @@
+def handle_e_command(year, dir_path):
+    """
+    This function parses files for given year and gives highest temperature, lowest temperature and max humiidty along with dates
+
+    Params:
+    - year: year for which data is to be displayed
+    - dir_path: directory where data is stored
+    """

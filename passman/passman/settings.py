dc1f5bc4678165f4d63df6bd2eba5e6cda28d888
Abdul Saboor <66201863+abdulsaboor938@users.noreply.github.com>
2024-07-19 00:19:07 +0500

    vercel-deployment-ready

--- passman/passman/settings.py
+++ passman/passman/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-0e!9_9n+d(m2v8t6!sj(co2v-gjm*$jk&k8s97657f@r64ec36
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".verce.app", "127.0.0.1"]
 
 
 # Application definition
--- temp.py
+++
@@ -1,4 +0,0 @@
-#  print interpreter path
-import sys
-
-print(sys.executable)
